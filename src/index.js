eece96ddf5866a93073e5d618841a3f8723ccde9
Camila Lobos <clobos@blackay.com>
2021-10-12 09:59:51 -0300

    cleaning code

--- src/index.js
+++ src/index.js
@@ -3,15 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import { Provider } from 'react-redux'
-// import generateStore from './redux/store'
-
-// const store = generateStore()
 
 ReactDOM.render(
-  // <Provider store={store}>
     <App />
-  // </Provider>
   , document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
